f4b1a0a35cfc9f48a015d533ffed1f13bc87dfb4
James Kerr <kerr@hey.com>
2022-06-23 14:06:34 -0700

    Made bump and inject scripts

---
+++ inject.js
@@ -0,0 +1,10 @@
+import { InsidersPackager } from "./packager.js"
+
+const appDir = process.argv[2]
+if (!appDir) {
+    console.log("Missing arg: provide the relative path to the app dir")
+    process.exit(1)
+}
+
+const pkg = new InsidersPackager(appDir)
+pkg.injectAppPackageJSON()
